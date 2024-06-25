478d31f4b26fdd96dee37bb02434788798751b8c
Martin Kasznár <91477677+xkasznar@users.noreply.github.com>
2024-06-25 05:26:56 +0200

    v0.976 w Slovak guide

--- client.py
+++ client.py
@@ -1,8 +1,13 @@
-# How to get IMPO App running?
-# 1. Install latest Python
-# 2. Write in command prompt: pip install scipy, after that pip install numpy
-# 3. First launch client.py and press button 1 or 2 with prepared .mat or .txt substrate
-# 4. Env is set up, button 3 is just for reading the old substrate, 1 and 2 are for replacing substrate
+# EN:How to get IMPO App running?
+# Install latest Python
+# Write in command prompt: pip install scipy, after that pip install numpy
+# First launch client.py and press button 1 or 2 with prepared .mat or .txt substrate
+# Env is set up, button 3 is just for reading the old substrate, 1 and 2 are for replacing substrate
+# SK:Ako spustiť aplikáciu IMPO?
+# Nainštalujte najnovší Python
+# Do príkazového riadka napíšte: pip install scipy, potom pip install numpy
+# Najprv spustite client.py a stlačte tlačidlo 1 alebo 2 s pripraveným .mat alebo .txt substrátom
+# Prostredie je nastavené, tlačidlo 3 slúži len na čítanie starého substrátu, 1 a 2 sú na výmenu substrátu
 import tkinter
 from tkinter import filedialog
 import turtle
